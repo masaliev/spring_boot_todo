016519339aff5570e34687ee2ee4aea18073ea74
Berdimurat Masaliev <muratmbt@gmail.com>
2018-11-14 13:40:07 +0600

    Initial commit

---
+++ backend/src/main/java/com/github/masaliev/todo/TodoApplication.java
@@ -0,0 +1,14 @@
+package com.github.masaliev.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApplication.class, args);
+    }
+}
