b337edabbe82bb7eaca725b9fe976ba188fdd978
Berdimurat Masaliev <muratmbt@gmail.com>
2018-11-29 11:51:33 +0600

    Add passport project

---
+++ passport/src/main/java/com/github/masaliev/passwort/PasswortApplication.java
@@ -0,0 +1,12 @@
+package com.github.masaliev.passwort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswortApplication.class, args);
+	}
+}
---
+++ passport/src/test/java/com/github/masaliev/passwort/PasswortApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.masaliev.passwort;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PasswortApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
